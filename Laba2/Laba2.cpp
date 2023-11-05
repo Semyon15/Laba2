167be188738abd14449cb522ce33f1aea9dc5eaa
Semyon Veretennikov <1132226170@pfur.ru>
2023-11-06 00:04:52 +0300

    Добавьте файлы проекта.

---
+++ Laba2/Laba2.cpp
@@ -0,0 +1,153 @@
+﻿#include <iostream>
+#include <vector>
+#include <random>
+#include <numeric>
+#include <map>
+void cout_index();
+double abg_computing();
+double moda_computing();
+double mediana_computing();
+void initializeVectors() {
+	short choice;
+	std::vector<std::string> names;
+	std::vector<int> grades;
+	std::cout << "Что вы хотите сделать?" << std::endl;
+	std::cout << "Введите 1 для демонстрационного варианта" << std::endl;
+	std::cout << "Введите 2 для случаной инициализации" << std::endl;
+	std::cout << "Введите 3 для самостоятельного ввода" << std::endl;
+	if (choice == 1) {
+		names = { "Antony", "Carmela", "Madow", "Kristopher", "Poly", "Jackie", "Alex", "Frank", "Ralph", "Junior"};
+		grades = { 90, 98, 95, 75, 55, 65, 89, 87, 78, 94 };
+	}
+	else if (choice == 2) {
+		std::cout << "Введите желаем размер: " << std::endl;
+		short size;
+		std::cin >> size;
+		std::vector<short> random_grades(size);
+		srand(time(NULL));
+		for (int i = 0; i < size; i++) {
+			random_grades[i] = rand() % 101;
+			
+		}
+	}
+	else if (choice == 3) {
+		short size, grade;
+		std::string name;
+		std::cout << "Введите количество студентов: " << std::endl;
+		std::cin >> size;
+		for (int i = 0; i < size; i++) {
+			std::cout << "Вводите имена: " << std::endl;
+			std::cin >> name;
+			names.push_back(name);
+			std::cout << "Вводите оценки: " << std::endl;
+			std::cin >> grade;
+			grades.push_back(grade);
+
+		}
+	}
+	
+}
+
+void cout_index(std::vector<std::string>&names, std::vector<short>&grades) {
+	for (int i = 0; i < names.size(); i++) {
+		std::cout << names[i] << " , ";
+
+	}
+	std::cout << std::endl; 
+	for (int i = 0; i < grades.size(); i++) {
+		std::cout << grades[i] << " , "; 
+	}
+}
+void cout_iterator(std::vector<std::string>& names, std::vector<short>& grades) {
+	for (auto i = names.begin(); i != names.end(); i++)
+		std::cout << *i << " , ";
+	std::cout << std::endl;
+	for (auto i = grades.begin(); i != grades.end(); i++)
+		std::cout << *i << " , ";
+	std::cout << std::endl;
+
+}
+
+void cout_RangeBasedFor(std::vector<std::string>& names, std::vector<short>& grades) { //без использования индексов
+	for (std::string name : names) 
+		std::cout << name << " , ";
+	std::cout << std::endl;
+	for (int grade : grades)
+		std::cout << grade << " , ";
+	std::cout << std::endl;
+}
+
+
+double avg_computing(std::vector<short>& grades) // note: the average must not be an integer
+{
+	return grades.empty() ? 0.0 :
+		std::accumulate(grades.begin(), grades.end(), 0.0)) / grades.size();
+}
+
+
+
+
+//поменять short на int (если не будет работать)
+
+double mediana_computing(std::vector<short>& grades) {
+	short size;
+	if (grades.empty())
+		return 0;
+	std::sort(grades.begin(), grades.end());
+	return (size % 2 == 0) ? (double)(grades[(size / 2) - 1] + grades[(size / 2)]) / 2.0 : (double)grades[size / 2];
+}
+
+
+double moda_computing(std::vector<short>& grades) {
+	short mode = grades[0];
+	short maxgrade = 1;
+	std::sort(grades.begin(), grades.end());
+	short count = 1;
+	for (int i = 0; i < grades.size(); i++) {
+		if (grades[i] < grades[i - 1])
+			count++;
+		else {
+			if (count > maxgrade) {
+				maxgrade = count;
+				mode = grades[i - 1];
+
+			}
+			count = 1;
+		}
+	}
+	if (count > maxgrade)
+		mode = grades[grades.size() - 1];
+	std::cout << "Мода: " << mode << std::endl;
+	//std::map <short, short> gradesfreq; //ключи - оценки, значения - кол-во раз, которое они встречаются
+	//for (short grade : grades) { //перебираю все оценки
+	//	gradesfreq[grade]++;
+	//}
+	//short 
+}
+
+int main() {
+	initializeVectors();
+	short choice; 
+	while (true) {
+		std::cout << "1) Вывести векторы методом index" << std::endl;
+		std::cout << "2) Вывести веторы методом Iterator" << std::endl;
+		std::cout << "3) Вывести векторы методом Range-based for-loop" << std::endl;
+		std::cout << "4) Вычислить медиану оценок" << std::endl;
+		std::cout << "5) Вычислить моду оценок" << std::endl;
+		std::cout << "6) Вычислить среднее значение оценок" << std::endl;
+		std::cout << "0) Выход" << std::endl;
+		std::cin >> choice;
+		if (choice == 0) {
+			break;
+		}
+		switch (choice) {
+			case 1: cout_index(); break;
+			case 2:cout_iterator(); break;
+			case 3:cout_RangeBasedFor(); break;
+			case 4:mediana_computing(); break;
+			case 5:moda_computing(); breal;
+			case 6:c
+				
+		}
+	}
+}
